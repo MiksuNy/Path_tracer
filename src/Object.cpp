307c2837ccb56bc74c4506c6d8bf5b3df5ac4118
Mikael Nyman <miksunyman1000@gmail.com>
2024-05-06 20:09:21 +0300

    Added Cube and Plane classes

--- src/Object.cpp
+++ src/Object.cpp
@@ -5,3 +5,13 @@ Object::Sphere::Sphere(glm::vec3 pos, float rad)
     position = pos;
     radius = rad;
 }
+
+Object::Plane::Plane(glm::vec3 pos)
+{
+    position = pos;
+}
+
+Object::Cube::Cube(glm::vec3 pos)
+{
+    position = pos;
+}
